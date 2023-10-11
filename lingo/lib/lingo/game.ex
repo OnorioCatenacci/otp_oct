39243093473977361ffb5860ba116e8e4f04c48b
Onorio Catenacci <catenacci@ieee.org>
2023-10-11 16:00:42 -0400

    Adding show of the results

--- lingo/lib/lingo/game.ex
+++ lingo/lib/lingo/game.ex
@@ -1,5 +1,6 @@
 defmodule Lingo.Game do
   alias Lingo.Word
+  alias IO.ANSI
 
   defstruct [
     :answer,
@@ -26,8 +27,24 @@ defmodule Lingo.Game do
     %{game | guesses: [score | game.guesses]}
   end
 
-  def show(game) do
-    ""
+  def show(%__MODULE__{guesses: g}) do
+    [score | _] = g
+
+    score
+    |> Enum.reduce("", fn e, acc -> acc <> show_letter(e) end)
+    |> IO.puts()
+  end
+
+  defp show_letter({l, :green} = {l, _color}) do
+    ANSI.black_background() <> ANSI.green() <> l <> ANSI.reset()
+  end
+
+  defp show_letter({l, :yellow} = {l, _color}) do
+    ANSI.black_background() <> ANSI.yellow() <> l <> ANSI.reset()
+  end
+
+  defp show_letter({l, :black} = {l, _color}) do
+    ANSI.black_background() <> ANSI.light_black() <> l <> ANSI.reset()
   end
 
   # Check if move is legal
