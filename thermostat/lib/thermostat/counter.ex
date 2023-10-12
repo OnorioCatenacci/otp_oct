9892c6e996795014a799b2c311ea2dbd659c627e
Saurav Singh <saurav0xb@users.noreply.github.com>
2023-10-12 08:45:30 -0700

    Create genserver for Thermostat

--- thermostat/lib/thermostat/counter.ex
+++ thermostat/lib/thermostat/counter.ex
@@ -1,11 +1,13 @@
 defmodule Thermostat.Counter do
-   def new(input) do
+  def new(input) do
     String.to_integer(input)
-   end
-   def add(acc, i) do
-    acc + i
-   end
-   def show(acc) do
-    to_string(acc)
-   end
   end
+
+  def add(acc, i) do
+    acc + i
+  end
+
+  def show(acc) do
+    to_string(acc)
+  end
+end
